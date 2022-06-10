329a32d7aaacf65fae73b534004b6e9f6f042605
nehanaisha <nehachaurasia682@gmail.com>
2022-06-10 14:58:32 +0530

    lastname

--- src/regex/com/Username.java
+++ src/regex/com/Username.java
@@ -5,17 +5,38 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 
 public class Username {
+	Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
+		Username obj = new Username();
+		obj.firstname();
+		obj.lastname();
+	}
+
+	void firstname() {
 		Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]*${3}");
-		System.out.println("enter the username");
+		System.out.println("enter the firstname");
 		String name = sc.nextLine();
 		Matcher matcher = pattern.matcher(name);
 		if (matcher.matches()) {
 			System.out.println("Username " + name + " is valid");
 		} else {
 			System.out.println("Username " + name + " is invalid");
+		}
+	}
+
+	void lastname() {
+		Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]*${4}");
+		System.out.println("enter the lastname");
+		String name = sc.nextLine();
+		Matcher matcher = pattern.matcher(name);
+		if (matcher.matches()) {
+			System.out.println("lastname " + name + " is valid");
+		} else {
+			System.out.println("lastname " + name + " is invalid");
 
 		}
+
 	}
 }
